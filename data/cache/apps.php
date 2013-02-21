a9f45c95844bf29fc837ab741c295c158f3fc516
Magillusionalgame <fantasyoui@gmail>
2013-02-21 18:45:04 +0800

    ..

---
+++ data/cache/apps.php
@@ -0,0 +1,39 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => '试炼人生',
+    'url' => 'http://www.tryanderror.cn/bbs',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  6 => 
+  array (
+    'appid' => '6',
+    'type' => 'DISCUZX',
+    'name' => '写意人生',
+    'url' => 'http://www.makeitcount.cn/bbs',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+);
